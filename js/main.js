8e0d76ae39ed6a42ae383556c08aef8c03e5cb53
Roman <romas6ka@i.ua>
2020-02-15 11:10:06 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -35,9 +35,9 @@ $(function() {
     e.preventDefault();
 
     if (e.originalEvent.deltaY < 0) {
-      $(this).slick("slickNext");
-    } else {
       $(this).slick("slickPrev");
+    } else {
+      $(this).slick("slickNext");
     }
   });
 
