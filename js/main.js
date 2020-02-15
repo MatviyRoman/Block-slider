fb874bc2076336391ac8b6c5a4cde45392c29545
Roman <romas6ka@i.ua>
2020-02-15 11:05:49 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(function() {
+  const slider = $("#slider");
+  slider.slick({
+    dots: true,
+    infinite: false,
+    autoplay: false,
+    arrows: true,
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    dotsClass: "myDotsPage",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        settings: {
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  slider.on("wheel", function(e) {
+    e.preventDefault();
+
+    if (e.originalEvent.deltaY < 0) {
+      $(this).slick("slickNext");
+    } else {
+      $(this).slick("slickPrev");
+    }
+  });
+
+  $("#btnOpen").on("click", function() {
+    $("#menu").slideToggle();
+  });
+});
+
+// if (location.host !== "rogue-chefs.matviy.pp.ua") {
+//   document.body.innerHTML = "";
+// }
